ef629e9370beedf0f26c8807c107abbff5b375d7
Pankaj Kumar Singh <pankajks.ece@gmail.com>
2018-02-15 09:00:54 +0530

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,3 @@
+const name = "Pankaj Kumar Singh";
+
+console.log(name);
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    entry: './assets/js/scripts.js', // we can have many entry points nut one output point
+    
+    output: {filename: './public/js/bundle123.js'}
+};
