bbfb89f64069fceb989c2c2a2b04b8756b54c534
Pankaj K S <SPankaj@ESTUATE.COM>
2018-02-23 19:01:33 +0530

    webpack file updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     entry: './assets/js/scripts.js', // we can have many entry points nut one output point
     
-    output: {filename: './public/js/bundle123.js'}
+    output: {filename: './public/js/bundle.js'}
 };
